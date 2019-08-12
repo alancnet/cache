be094fccc10dbace5b3272ff1d82a54180252f05
Alan Colon <alan.colon@cybercoders.com>
2019-08-12 13:05:06 -0700

    Add garbage collection

--- index.js
+++ index.js
@@ -4,12 +4,33 @@
 function Cache(ttl) {
 
 	var me = this;
+	var head = null;
+	var tail = null;
 
 	me.now = function() { return (new Date()).getTime() }
 	me.ttl = ttl || 0;
 	me.data = {}
 
+	me.gc = function() {
+		if (head && me.now() >= head.expires) {
+			delete me.data[head.key];
+			head = head.next;
+			me.gc();
+		}
+	}
+
+	me.keys = function() {
+		var keys = []
+		var obj = head
+		while (obj) {
+			keys.push(obj.key)
+			obj = obj.next
+		}
+		return keys
+	}
+
 	me.get = function(key) {
+		me.gc()
 		var val = null
 		var obj = me.data[key]
 		if(obj) {
@@ -23,18 +44,28 @@ function Cache(ttl) {
 	}
 
 	me.del = function(key) {
-		var oldval = me.get(key); 
-		delete me.data[key]
-		return oldval
+		me.gc()
+		var oldEntry = me.data[key];
+		if (oldEntry) {
+			if (oldEntry.prev) oldEntry.prev.next = oldEntry.next
+			if (oldEntry.next) oldEntry.next.prev = oldEntry.prev
+			delete me.data[key]
+			return oldEntry.val
+		}
 	}
 
 	me.put = function(key, val, ttl) {
+		me.gc()
 		if(ttl === undefined) {
 			ttl = me.ttl;
 		}
 		var oldval = me.del(key); 
-		if(val !== null)
-			me.data[ key ] = { expires: me.now() + ttl, val: val }
+		if(val !== null) {
+			var next = { key: key, expires: me.now() + ttl, val: val, prev: tail }
+			if (tail) tail = tail.next = next
+			else head = tail = next
+			me.data[ key ] = next
+		}
 		return oldval
 	}
 
